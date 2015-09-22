bc50052667fa6dace9e3a108fcfede375ee145fd
Frieder Griesshammer <frieder.griesshammer@der-beweis.de>
2015-09-22 23:04:09 +0200

    rewriting most parts of the script

--- locate_me.lua
+++ locate_me.lua
@@ -4,46 +4,50 @@ local device = "/dev/ttyACM0"
 local verbose = false
 local use_alt = false
 
-local util = require('luci.util')
-local uci = require('luci.model.uci').cursor()
+local util = require("luci.util")
+local uci = require("luci.model.uci").cursor()
 
 
-function parse_args()
-    function usage()
-        print("\nusage: " .. arg[0] .. " [-ahv] [device]")
-        print("-a", "use altitude")
-        print("-v", "verbose output")
-        print("-h", "print help")
-        print("device is: '" .. device .. "'")
-    end
-
-    local i = 1
-    while arg[i] do
-        if arg[i] == '-v' then
-            verbose = true
-        elseif arg[i] == '-a' then
-            use_alt = true
-        elseif arg[i] == '-h' then
-            usage()
-            os.exit(0)
-        elseif string.sub(arg[i], 0, 1) == '/' then
-            device = arg[i]
-        else
-            io.stderr:write("error parsing command line")
-            usage()
-            os.exit(1)
-        end
-        i = i + 1
-    end
+local function msg(txt, nor)
+    local pfunc = nor and io.stdout or io.stderr
+    pfunc:write(txt .. "\n")
 end
 
 
-function get_location(device)
+local function parse_args()
+    local function usage()
+        msg("\nusage: " .. arg[0] .. " [-avh] [device]", true)
+        msg("-a\tset altitude, too", true)
+        msg("-v\tuse verbose output", true)
+        msg("-h\tprint this help", true)
+        msg("\ndevice is: " .. device .. "\n", true)
+    end
 
-    function conv(raw, bias, ltd)
+    for _, a in ipairs(arg) do
+        if a == "-v" or a == "--verbose" then
+            verbose = true
+        elseif a == "-a" or a == "--altitude" then
+            use_alt = true
+        elseif a == "-h"  or a == "--help" then
+            usage()
+            os.exit(0)
+        elseif string.sub(a, 1, 1) == '/' then -- string.startswith()
+            device = a
+        else
+            msg("could not parse args")
+            usage()
+            os.exit(1)
+        end
+    end
+
+end
+
+
+local function get_location(device_path)
+    local function conv(raw, bias, ltd)
         -- converting NMEA latitude & longitude to decimal
         local sp = ltd and 2 or 3
-        -- are we converting a latitude or longitude here?
+        -- is this a latitude or longitude value here?
         -- so chomp of the first two or three digits for the degrees
         local bs = (bias ~= "N" or bias ~= "E") and 1 or -1
         -- prefix location with a minus if location is in the south or west
@@ -55,91 +59,83 @@ function get_location(device)
     end
 
     local location = {}
-    local collecting = true
+    local handle = io.open(device_path, "r")
 
-    local handle = io.open(device, "r")
-
-    while collecting do
+    repeat
         local line = handle:read("*line")
         local data = util.split(line, ",")
 
         if data[1] == "$GPGGA" then
-            if data[7] ~= "0" then
+
+            if data[7] ~= "0" then -- no error in gps data
                 location.latitude = conv(data[3], data[4], true)
                 location.longitude = conv(data[5], data[6], false)
                 location.altitude = tonumber(data[10])
             else
-                io.stdout:write(":")
+                if verbose then io.stdout:write(":") end
             end
+
         elseif data[1] == "$GPRMC" then
-            if data[3] == "A" then
+
+            if data[3] == "A" then -- real gps fix
                 location.latitude = conv(data[4], data[5], true)
                 location.longitude = conv(data[6], data[7], false)
-            else
-                io.stdout:write(":")
-            end
+           else
+               if verbose then io.stdout:write(":") end
+           end
 
         else
-            io.stdout:write(".")
+
+            if verbose then io.stdout:write(".") end
+
         end
 
-        if (location.latitude ~= nil) and (location.longitude ~= nil) and (use_alt and (location.altitude ~= nil)) then
-            collecting = false
-        end
-
-    end
-
-    handle:close()
+    until (
+        (location.latitude ~= nil) and (location.longitude ~= nil) and
+        (use_alt and (location.altitude ~= nil))
+    )
 
     return location
+
 end
 
 
-function save_location(location)
+local function save_location(location)
     local config = "gluon-node-info"
     local section = uci:get_first(config, "location")
 
     local share_location = uci:get_bool(config, section, "share_location")
 
+
     if location.latitude and not uci:set(config, section, "latitude", location.latitude) then
-        io.stderr:write("could not set latitude " .. location.latitude .. "\n")
-        return false
+        return msg("could not set latitude " .. location.latitude)
     end
     if location.longitude and not uci:set(config, section, "longitude", location.longitude) then
-        io.stderr:write("could not set longitude " .. location.longitude .. "\n")
-        return false
+        return msg("could not set longitude " .. location.longitude)
     end
-
     if location.altitude and not uci:set(config, section, "altitude", location.altitude) then
-        io.stderr:write("could not set altitude " .. location.altitude .. "\n")
-        return false
+        return msg("could not set altitude " .. location.altitude)
     end
-
 
     if not uci:save(config) then
-        io.stderr:write("could not save " .. config .. "\n")
-        return false
+        return msg("could not save " .. config)
     end
     if not uci:commit(config) then
-        io.stderr:write("could not commit " .. config .. "\n")
-        return false
+        return msg("could not commit " .. config)
     end
 
     if verbose and not share_location then
-        io.stdout:write("[warn] option 'share_location' is disabled\n")
+        msg("[warn] option 'share_location' is disabled", true)
     end
 
     return true
 end
 
-
--- do stuff here
 parse_args()
-
 if not save_location(get_location(device)) then
-    if verbose then io.stdout:write("[failed]\n") end
+    if verbose then msg("sorry, something failed") end
     os.exit(1)
-else
-    if verbose then io.stdout:write("[success]\n") end
-    os.exit(0)
 end
+
+if verbose then msg("all went well", true) end
+os.exit(0)
